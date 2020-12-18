be5331770164d1193e1dbfbd3a8efa427d5dcd40
LAPTOP-B8Q7IKF1\indra <indrayasya96@gmail.com>
2020-12-18 14:16:32 +0800

    membuat if conditiona untuk setiap menu. gagal

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
 var x = 0;
 
-const tambah = document.querySelectorAll('.tambah');
+const tambah = document.querySelector('.tambah');
 const kurang = document.querySelectorAll('.kurang');
-// var parent = tambah.parentNode.getAttribute("id");
+// let parent = tambah.parentNode.getAttribute("id");
 
-let makanan = {"kwetiau":0}
+let makanan = {}
 
-tambah.forEach(function(tbh){
-    tbh.addEventListener('click',function(){
+tambah.addEventListener('click',function(){
+    id = this.parentNode.getAttribute('id');
+    if(id == "kwetiau"){
         x = 0
-        kwetiau = Object.keys(makanan)[0]
-        id = this.parentNode.getAttribute(`${kwetiau}`)
-        let jumlah = document.querySelector(`${kwetiau}`)
-        // jumlah.textContent = x++
-        // console.log(makanan.kwetiau++)
-        jumlah.textContent = makanan.kwetiau++
-        // console.log(makanan.kwetiau)
-    })
+        jumlah = document.querySelector('.kwetiau')
+        jumlah.textContent = x++
+    }
 })
 
-kurang.forEach(function(tbh){
-    tbh.addEventListener('click',function(){
-        id = this.parentNode.getAttribute("id");
-        console.log(id)
-        let jumlah = document.querySelector(`.${id}`)
-        jumlah.textContent = x--
-        if(jumlah.textContent == 0){
-            return x = 0;
-        }
-    })
-})
-
-
-// for(const ttambah of tambah){
-//     ttambah.addEventListener('click',function(){
-//         id = this.parentNode.getAttribute("id");
-//         console.log(id)
-//         let jumlah = document.querySelector(`.${id}`)
-//         jumlah.textContent = x++
-//     })
-// }
-
-// for(const tkurang of kurang){
-//     tkurang.addEventListener('click',function(){
+// kurang.forEach(function(tbh){
+//     tbh.addEventListener('click',function(){
 //         id = this.parentNode.getAttribute("id");
 //         console.log(id)
 //         let jumlah = document.querySelector(`.${id}`)
@@ -51,19 +25,4 @@ kurang.forEach(function(tbh){
 //             return x = 0;
 //         }
 //     })
-// }
-
-
-
-// tambah.addEventListener('click',function(){
-//     let jumlahKK = document.querySelector('#kk');
-//     jumlahKK.textContent = x++
-// })
-
-// kurang.addEventListener('click',function(){
-//     let jumlahKK = document.querySelector('#kk');
-//     jumlahKK.textContent = x--
-//     if(jumlahKK.textContent == 0){
-//         return x = 0
-//     }
 // })
